e24959499a3369bbe1f6dec31ff9024a09827120
Tobias Roti <tobiasroti@Tobiass-MacBook-Pro.local>
2019-11-07 21:35:57 +0100

    GroundZero

--- main.py
+++ main.py
@@ -13,7 +13,14 @@ class ScanDelegate(DefaultDelegate):
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(10.0)
 
+arudino = ''
+
+
 for dev in devices:
     print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+    if dev.addr == 'dd:db:26:00:0c:86':
+        print("FUCK")
     for (adtype, desc, value) in dev.getScanData():
         print "  %s = %s" % (desc, value)
+
+while True:
