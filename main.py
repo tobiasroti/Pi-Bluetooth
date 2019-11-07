fe32f0a4e15a2686604ee3c64aced028184f93b3
Tobias Roti <tobiasroti@dhcp-10-22-68-149.wlan.ntnu.no>
2019-11-07 20:34:46 +0100

    GroundZero

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
- from bluepy import btle
+from bluepy import btle
 
- print "Connecting..."
- dev = btle.Peripheral("B0:B4:48:BF:C9:83")
+print "Connecting..."
+dev = btle.Peripheral("B0:B4:48:BF:C9:83")
 
- print "Services..."
- for svc in dev.services:
-     print str(svc)
+print "Services..."
+for svc in dev.services:
+ print str(svc)
