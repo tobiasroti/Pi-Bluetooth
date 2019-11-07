3de662ffbfc28c21d0d2d35b92cbc90da8acb84d
Tobias Roti <tobiasroti@dhcp-10-22-68-149.wlan.ntnu.no>
2019-11-07 20:49:17 +0100

    GroundZero

--- main.py
+++ main.py
@@ -158,6 +158,8 @@ class BTInterface(btle.DefaultDelegate):
         self.wakecpu.write('\x01', withResponse=True)
 
     def getSpheroCharacteristic(self):
+        print(self.peripheral.getCharacteristics(uuid='6E400001-B5A3-F393-E0A9-E50E24DCCA9E'))
+        print('FUCK')
         return self.peripheral.getCharacteristics(uuid='6E400001-B5A3-F393-E0A9-E50E24DCCA9E')
 
     def dumpCharacteristics(self):
