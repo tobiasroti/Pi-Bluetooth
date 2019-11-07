47b6b350f5528a1b15af940b0e84202a39355a9f
Tobias Roti <tobiasroti@Tobiass-MacBook-Pro.local>
2019-11-07 17:09:14 +0100

    Scanning test

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-from bluepy.btle import Scanner, DefaultDelegate
+from bluepy import btle
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
+class Bluetooth:
 
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print "Discovered device", dev.addr
-        elif isNewData:
-            print "Received new data from", dev.addr
+    def __init__(self, deviceAddress):
+            self.deviceAddress = deviceAddress
+            btle.DefaultDelegate.__init__(self)
 
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
+            # Address type must be "random" or it won't connect.
+            self.peripheral = btle.Peripheral(deviceAddress, btle.ADDR_TYPE_RANDOM)
+            self.peripheral.setDelegate(self)
 
-for dev in devices:
-    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-    for (adtype, desc, value) in dev.getScanData():
-        print "  %s = %s" % (desc, value)
+            self.seq = 0
+
+            # Attribute UUIDs are identical to Ollie.
+            self.antidos = self.getSpheroCharacteristic('2bbd')
+            self.wakecpu = self.getSpheroCharacteristic('2bbf')
+            self.txpower = self.getSpheroCharacteristic('2bb2')
+            self.roll = self.getSpheroCharacteristic('2ba1')
+            self.notify = self.getSpheroCharacteristic('2ba6')
+
+            # This startup sequence is also identical to the one for Ollie.
+            # It even uses the same unlock code.
+            print 'Sending antidos'
+            self.antidos.write('011i3', withResponse=True)
+            print 'Sending txpower'
+            self.txpower.write('\x0007', withResponse=True)
+            print 'Sending wakecpu'
+            self.wakecpu.write('\x01', withResponse=True)
+
+test = Bluetooth("DD:DB:26:00:0C:86")
