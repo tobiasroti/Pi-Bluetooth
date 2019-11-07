cd168363b7b9cf97b7664468321da76c999454e2
Tobias Roti <tobiasroti@Tobiass-MacBook-Pro.local>
2019-11-07 17:17:50 +0100

    Scanning test

--- main.py
+++ main.py
@@ -10,21 +10,6 @@ class BTInterface(btle.DefaultDelegate):
             self.peripheral.setDelegate(self)
 
             self.seq = 0
-'''
-            # Attribute UUIDs are identical to Ollie.
-            self.antidos = self.getSpheroCharacteristic('2bbd')
-            self.wakecpu = self.getSpheroCharacteristic('2bbf')
-            self.txpower = self.getSpheroCharacteristic('2bb2')
-            self.roll = self.getSpheroCharacteristic('2ba1')
-            self.notify = self.getSpheroCharacteristic('2ba6')
-'''
-            # This startup sequence is also identical to the one for Ollie.
-            # It even uses the same unlock code.
-            #print('Sending antidos')
-            self.antidos.write('011i3', withResponse=True)
-            #print('Sending txpower')
-            self.txpower.write('\x0007', withResponse=True)
-            #print('Sending wakecpu')
-            self.wakecpu.write('\x01', withResponse=True)
+
 
 test = BTInterface("DD:DB:26:00:0C:86")
