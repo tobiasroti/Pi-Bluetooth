c12538359e14ab4fe9507d48fbe0a3208eae850e
Tobias Roti <tobiasroti@Tobiass-MacBook-Pro.local>
2019-11-07 20:52:33 +0100

    GroundZero

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ class BTInterface(btle.DefaultDelegate):
         self.wakecpu.write('\x01', withResponse=True)
 
     def getSpheroCharacteristic(self):
-        print(self.peripheral.getCharacteristics(uuid='6E400001-B5A3-F393-E0A9-E50E24DCCA9E'))
+        print(self.peripheral.getCharacteristics(startHnd=1, endHnd=0xFFFF, uuid=None))
         print('FUCK')
         return self.peripheral.getCharacteristics(uuid='6E400001-B5A3-F393-E0A9-E50E24DCCA9E')
 
