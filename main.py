586572ac6fe02fb43dae5a8b9c0da8f69e015fcd
Tobias Roti <tobiasroti@Tobiass-MacBook-Pro.local>
2019-11-07 20:56:48 +0100

    GroundZero

--- main.py
+++ main.py
@@ -165,7 +165,8 @@ class BTInterface(btle.DefaultDelegate):
             "parent": "Uncategorized",
             "uuid": "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
             }
-        print uuid.UUID(o['uuid']).hex
+        test = uuid.UUID(o['uuid']).hex
+        print(test)
         print('FUCK')
         return self.peripheral.getCharacteristics(uuid='6E400001-B5A3-F393-E0A9-E50E24DCCA9E')
 
